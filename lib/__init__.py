9254a2126cd18d01156116651f8641a1996ec375
SNunes <solangesilnunes@gmail.com>
2017-12-01 17:58:32 +0000

    added lib

---
+++ lib/__init__.py
@@ -0,0 +1,15 @@
+"""Python Bayesian Networks
+
+"""
+
+__version__ = '1.1'
+
+
+try:
+    import numpy as np
+except ImportError:
+    raise ImportError('NumPy does not seem to be installed. Please see the user guide.')
+
+from network import *
+from operations import *
+from hdf5_getters import *
